0a158e3261bb8ab2e0e4647687764182d2597052
nihalkaftan <nihalkaftan@gmail.com>
2020-02-29 15:37:05 -0500

    looping through counter

---
+++ src/day01/LoopPractice.java
@@ -0,0 +1,10 @@
+package day01;
+
+public class LoopPractice {
+    public static void main(String[] args) {
+        //some random code
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Counter :" + 1);
+        }
+    }
+}
